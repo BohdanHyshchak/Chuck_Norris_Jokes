c5eec62d5697a53a1434be27cbf5f97a74ca8311
Bohdan Hyshchak <bogdangisak@gmail.com>
2021-08-20 17:05:15 +0300

    last commit before big changes_2

--- app/src/main/java/com/example/retrofit_1/ui/MainActivity.kt
+++ app/src/main/java/com/example/retrofit_1/ui/MainActivity.kt
@@ -3,14 +3,17 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import com.example.retrofit_1.api.RetrofitInstance
 import com.example.retrofit_1.databinding.ActivityMainBinding
+import com.example.retrofit_1.repository.JokeRepository
 import retrofit2.HttpException
 import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var viewModel: MainViewModel
     val TAG = "MainActivity"
 
     private lateinit var binding: ActivityMainBinding
@@ -20,23 +23,41 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        val jokeRepository = JokeRepository()
+        val viewModelProviderFactory = MainViewModelProviderFactory(jokeRepository)
+        viewModel = ViewModelProvider(this, viewModelProviderFactory).get(MainViewModel::class.java)
     }
-    fun nextJoke(view: View) {
-        lifecycleScope.launchWhenCreated {
-            val response = try {
-                RetrofitInstance.api.getJoke()
-            } catch (e: IOException) {
-                Log.e(TAG, "IOException")
-                return@launchWhenCreated
-            } catch (e: HttpException) {
-                Log.e(TAG, "HttpException")
-                return@launchWhenCreated
-            }
-            if (response.isSuccessful && response.body() != null) {
-                binding.tvTextJoke.text = response.body()!!.value
-            } else {
-                Log.e(TAG, "Response was not successful")
-            }
-        }
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    fun nextJoke(view: View) {
+//        lifecycleScope.launchWhenCreated {
+//            val response = try {
+//                RetrofitInstance.api.getJoke()
+//            } catch (e: IOException) {
+//                Log.e(TAG, "IOException")
+//                return@launchWhenCreated
+//            } catch (e: HttpException) {
+//                Log.e(TAG, "HttpException")
+//                return@launchWhenCreated
+//            }
+//            if (response.isSuccessful && response.body() != null) {
+//                binding.tvTextJoke.text = response.body()!!.value
+//            } else {
+//                Log.e(TAG, "Response was not successful")
+//            }
+//        }
+//    }
 }
--- app/src/main/java/com/example/retrofit_1/ui/fragments/JokeFragment.kt
+++ app/src/main/java/com/example/retrofit_1/ui/fragments/JokeFragment.kt
@@ -1,7 +1,26 @@
 package com.example.retrofit_1.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.retrofit_1.R
+import com.example.retrofit_1.ui.MainActivity
+import com.example.retrofit_1.ui.MainViewModel
 
 class JokeFragment : Fragment(R.layout.activity_main) {
+
+    lateinit var viewModel: MainViewModel
+
+    val TAG = "Joke Fragment"
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = (activity as MainActivity).viewModel
+        setupUI()
+    }
+
+    private fun setupUI() {
+        
+    }
 }
